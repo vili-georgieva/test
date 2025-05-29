dc0915f9e47f509afe7cdd0d80d688a2f0951f63
Velichka Georieva <georgieva@tutanota.de>
2025-05-29 15:54:09 +0200

    second  test case Maria

--- Electrons around the cores/string_calculator/spec/string_calculator_spec.js
+++ Electrons around the cores/string_calculator/spec/string_calculator_spec.js
@@ -10,4 +10,8 @@ describe('String Calculator', function() {
 	it('should return 6 for [ 1, 2, 3, 4, 5 ]', function() {
 		expect(calculator.add([ 1, 2, 3, 4, 5 ])).toEqual(6);
 	});
+
+	it('should return 4 for [ 2, 2, 3, 3 ]', function() {
+		expect(calculator.add([ 2, 2, 3, 3 ])).toEqual(4);
+	});
 });
