451d33faf4b0ee10e9c9e0fd9156614a26c0f99b
Velichka Georieva <georgieva@tutanota.de>
2025-05-29 16:19:53 +0200

    second  prod code Velichka

--- Electrons around the cores/string_calculator/src/string_calculator.js
+++ Electrons around the cores/string_calculator/src/string_calculator.js
@@ -1,5 +1,13 @@
 // StringCalculator.js
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
-	return 6;
+	let sum= 0;
+	for(let i = 0; i < string_numbers.length; i++) {
+		if(string_numbers[i] > 2) {
+			sum += 2;
+		}	
+		
+
+	}
+	return sum;
 };
