a3eb0afde4b96852b5bc9d442841fb272ec5da56
Velichka Georieva <georgieva@tutanota.de>
2025-05-29 15:50:55 +0200

    first testcase Velichka

--- Electrons around the cores/string_calculator/spec/string_calculator_spec.js
+++ Electrons around the cores/string_calculator/spec/string_calculator_spec.js
@@ -3,7 +3,11 @@ describe('String Calculator', function() {
 		calculator = new StringCalculator();
 	});
 
-	it('should return 0 for an empty string', function() {
+	/*it('should return 0 for an empty string', function() {
 		expect(calculator.add('')).toEqual(0);
+	});*/
+
+	it('should return 6 for [ 1, 2, 3, 4, 5 ]', function() {
+		expect(calculator.add([ 1, 2, 3, 4, 5 ])).toEqual(6);
 	});
 });
