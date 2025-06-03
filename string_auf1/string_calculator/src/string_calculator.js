dc4c7d38124f0916916e22c578139ae2ab787944
Velichka Georieva <georgieva@tutanota.de>
2025-06-03 21:20:34 +0200

    implement findSpaceShip() and also working logic

--- string_auf1/string_calculator/src/string_calculator.js
+++ string_auf1/string_calculator/src/string_calculator.js
@@ -1,22 +1,33 @@
 //const { map } = require("jquery");
 
 // StringCalculator.js
-//function StringCalculator() { }
 
 function findSpaceship(map) {
-	
-	return [x, y]
-
+	const rows = map.trim().split('\n');
+	const totalRows = rows.length;
+  
+	for (let i = 0; i < totalRows; i++) {
+	  const row = rows[i];
+	  const x = row.indexOf('X');
+	  if (x !== -1) {
+		const y = totalRows - 1 - i;
+		return [x, y];
+	  }
+	}
+  
+	return "Spaceship lost forever.";
 }
+function StringCalculator() { }
 StringCalculator.prototype.add = function (string_numbers) {
 	let map = "..........\n" +
-		"..........\n" +
-		"..........\n" +
 		".......X..\n" +
+		"..........\n" +
+		"..........\n" +
 		"..........\n" +
 		"..........";
 
 	const [x_coordinate, y_coordinate] = findSpaceship(map);
 	// console log
 	//return [7, 2];
+	return [x_coordinate, y_coordinate];
 };
