8e8e8f32ab63c0c6544021f2ceec660b99fc8d60
Velichka Georieva <georgieva@tutanota.de>
2025-06-03 20:23:49 +0200

    logic for counting height and width

--- string_auf1/string_calculator/src/string_calculator.js
+++ string_auf1/string_calculator/src/string_calculator.js
@@ -1,6 +1,15 @@
 // StringCalculator.js
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
-	//
-	return [1, 1];
+	let height = 0;
+	let width = 0;
+	for (let i = 0; i < string_numbers.length; i++) {
+		if (string_numbers[i] === '\n') {
+			height++;
+			width = 0;
+		} else {
+			width++;
+		}
+	}
+	return [width, height];
 };
